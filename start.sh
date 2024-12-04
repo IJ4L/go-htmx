83ca7d3f02f1e571d26dd7095881421aa0cd32cf
ijul <rijal9246@gmail.com>
2024-12-04 18:48:46 +0800

    fix: ci/cd pipeline

--- start.sh
+++ start.sh
@@ -6,8 +6,10 @@ echo "run db migration"
 
 if [ -f /app/app.env ]; then
     source /app/.env
+    echo "source /app/.env"
 fi
 
+echo "DB_SOURCE: $DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
