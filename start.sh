e2217b05cd81ad40a59fb6f824b0522038a48f55
ijul <rijal9246@gmail.com>
2024-12-04 23:30:51 +0800

    fix: ci/cd pipeline

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ set -e
 
 echo "run db migration"
 
-chmod 644 /app/app.env
+chmod 644 /app/.env
 
 ls -l
 
-source /app/app.env
+source /app/.env
 
 echo "DB_SOURCE: $DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
