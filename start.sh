1f43570c67f2d1a24063bbd80e3436215de1a8c1
ijul <rijal9246@gmail.com>
2024-12-04 20:30:28 +0800

    fix: ci/cd pipeline

--- start.sh
+++ start.sh
@@ -6,15 +6,10 @@ echo "run db migration"
 
 ls -l
 
-if [ -f /app/app.env ]; then
-    source /app/app.env
-    echo "source /app/app.env"
-else
-    echo "/app/app.env does not exist"
-fi
+source /app/app.env
 
 echo "DB_SOURCE: $DB_SOURCE"
-/app/migrate -path /app/migration -database postgresql://root:secret@postgres:5432/kruty_craft?sslmode=disable -verbose up
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
