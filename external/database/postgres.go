ea1de73a8da6669f545063ca24a6dbce112a5c0d
ijul <rijal9246@gmail.com>
2024-12-06 05:02:10 +0800

    fix: ci/cd pipeline

--- external/database/postgres.go
+++ external/database/postgres.go
@@ -14,7 +14,7 @@ func ConnectPostgreSql(data config.Config) (db *pgx.Conn, err error) {
 	conn, err := pgx.Connect(context.Background(), dsn)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		return nil, err
+		return nil, fmt.Errorf("Unable to connect to database: %v", err)
 	}
 
 	return conn, nil
