42d2175ff8b6935371d048f0a0f8d70ce3c868bd
ijul <rijal9246@gmail.com>
2024-12-06 07:22:35 +0800

    fix: ci/cd pipeline

--- apps/image.go
+++ apps/image.go
@@ -11,7 +11,7 @@ import (
 func serveImage(c *gin.Context) {
 	imageName := c.Param("imageName")
 
-	imagePath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/images", imageName)
+	imagePath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/images/", imageName)
 
 	if _, err := os.Stat(imagePath); os.IsNotExist(err) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "gambar tidak ditemukan"})
@@ -24,7 +24,7 @@ func serveImage(c *gin.Context) {
 func serveIcon(c *gin.Context) {
 	iconName := c.Param("iconName")
 
-	iconPath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/icons", iconName)
+	iconPath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/icons/", iconName)
 
 	if _, err := os.Stat(iconPath); os.IsNotExist(err) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "gambar tidak ditemukan"})
@@ -37,7 +37,7 @@ func serveIcon(c *gin.Context) {
 func serveStorage(c *gin.Context) {
 	fileName := c.Param("fileName")
 
-	filePath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/storages", fileName)
+	filePath := filepath.Join("/home/ijulyy/go-htmx/external/public/assets/storages/", fileName)
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "gambar tidak ditemukan"})
